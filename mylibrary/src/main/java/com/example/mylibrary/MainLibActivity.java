6f47b95bd91ff21499dd782719357163cd8de6fa
LeoKasaw <527047054@qq.com>
2023-05-04 14:52:37 +0800

    测试代码

---
+++ mylibrary/src/main/java/com/example/mylibrary/MainLibActivity.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainlib);
+    }
+}
